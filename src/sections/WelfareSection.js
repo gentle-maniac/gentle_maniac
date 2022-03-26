e6332ab64d13f8b82ab39f980f80eef0d40253fc
RokwonK <wonrok97@naver.com>
2022-03-26 23:01:40 +0900

    복지 파트 모바일 대응

--- src/sections/WelfareSection.js
+++ src/sections/WelfareSection.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
+import Image from "next/image";
+import { device } from "../styles";
+import { Img } from "../assets";
 import { SectionLayout } from "../components";
+
 
 export const WelfareSection = () => {
   return (
@@ -10,7 +14,10 @@ export const WelfareSection = () => {
         <div className="container">
           {welfareData.map((v, i) => (
             <WelfareItem key={i}>
-              <div className="icon" />
+              
+              <div className="icon">
+                <Image src={v.icon} alt="복지" priority />
+              </div>
               <div>
                 <div className="title">{v.title}</div>
                 <div className="content">{v.content}</div>
@@ -43,9 +50,11 @@ const WelfareItem = styled.div`
   & + * {
     margin-top: 2rem;
   }
+
   & > * + * {
     margin-left: 3rem;
   }
+
   .icon {
     width: 90px;
     height: 90px;
@@ -61,43 +70,66 @@ const WelfareItem = styled.div`
   .content {
     margin-top: 4px;
   }
+
+  @media ${device.mobile} { 
+    margin-left: 2rem;
+
+    & > * + * {
+      margin-left: 1rem;
+    }
+
+    .icon {
+      width: 60px;
+      height: 60px;
+    }
+
+    .title {
+      margin-top: 6px;
+      font-size: 1.0rem;
+      font-weight: 900;
+    }
+
+    .content {
+      margin-top: 8px;
+      font-size: 12px;
+    }
+  }
 `;
 
 const welfareData = [
   {
-    icon: "",
+    icon: Img.복지01,
     title: "[최신형 업무 장비]",
     content: "일의 효율을 높여요",
   },
   {
-    icon: "",
+    icon: Img.복지02,
     title: "[여유 있는 출근 시간]",
     content: "10시출근으로 복합한 시간대를 피해요",
   },
   {
-    icon: "",
+    icon: Img.복지03,
     title: "[인센티브 제도]",
     content: "성과에 대해선 확실하게 보장해요",
   },
   {
-    icon: "",
+    icon: Img.복지04,
     title: "[다양한 간식 제공]",
     content: "직원들이 좋아하는 걸로 준비해요",
   },
   {
-    icon: "",
-    title: "[쳥년내일채움 가입 적용]",
+    icon: Img.복지05,
+    title: "[청년내일채움 가입 적용]",
     content: "목돈마련 회사에서 정부에서 함께 도와줘요",
   },
   {
-    icon: "",
+    icon: Img.복지06,
     title: "[회식강요 워크샵 놉!]",
     content: "꼰대 문화 없어요",
   },
   {
-    icon: "",
+    icon: Img.복지07,
     title: "[건강을 위하여]",
-    content: `매월 1회 피자 or 버거파티\n
-    영양제 구비 직원 건강을 챙겨요`,
+    content: `매월 1회 피자 or 버거파티\n영양제 구비 직원 건강을 챙겨요`,
   },
 ];
