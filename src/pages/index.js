eff6a79deb0140c1fc64fe926aa22885383d21a0
RokwonK <wonrok97@naver.com>
2022-04-22 15:35:42 +0900

    대표 인사말 및 이미지 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Index() {
     <>
       <Menu scrollTo={scrollTo} />
       <SEO />
-      <Parallax className="parallax" ref={parallaxRef} pages={11.5}>
+      <Parallax className="parallax" ref={parallaxRef} pages={10.7}>
         <ParallaxLayer offset={0} speed={0.3}>
           <LogoSection />
         </ParallaxLayer>
@@ -31,43 +31,27 @@ export default function Index() {
           <HomeSection />
         </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1.6}
-          speed={0.3}
-          style={{ pointerEvents: "none" }}
-        >
-          <DirectorImage src="대표님01" />
-        </ParallaxLayer>
-
         <ParallaxLayer offset={2} speed={0.2}>
           <GreetingSection />
         </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={2.7}
-          speed={0.4}
-          style={{ pointerEvents: "none" }}
-        >
-          <DirectorImage src="대표님02" align="flex-end" />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={3} speed={0.2}>
+        <ParallaxLayer offset={3.3} speed={0.2}>
           <VisionSection />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={4.1} speed={0.1} factor={4}>
+        <ParallaxLayer offset={4.4} speed={0.1} factor={4}>
           <TalentSection />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={7} speed={0.4}>
+        <ParallaxLayer offset={7.0} speed={0.4}>
           <WelfareSection />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={8.9} speed={0.1}>
+        <ParallaxLayer offset={8.5} speed={0.1}>
           <MakingSection />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={11} speed={0.3}>
+        <ParallaxLayer offset={10.3} speed={0.3}>
           <Footer scrollToTop={() => scrollTo(0)} />
         </ParallaxLayer>
       </Parallax>
--- src/sections/GreetingSection.js
+++ src/sections/GreetingSection.js
@@ -1,76 +1,100 @@
 // @ts-nocheck
 import styled from "styled-components";
+import Image from "next/image";
+import { useRef } from "react";
+import { useSpring, animated } from "react-spring";
+import { useObserver } from "../utils";
 import { SectionLayout } from "../components";
 import { device } from "../styles";
+import { Img } from "../assets";
 
 export const GreetingSection = () => {
+  const titleRef = useRef(null);
+  const visible = useObserver(titleRef);
+
+  const titleStyle = useSpring({
+    opacity: visible ? 1 : 0,
+    x: visible ? 0 : -100,
+  });
+
   return (
     <SectionLayout theme="black" justify="flex-start">
       <Wrapper className="gold">
-        <h1>대표 인사말</h1>
-
-        <h3>복규동 대표</h3>
-        <br />
-        <p>
-          일반 취향을 가진 일반인 분들과 특수 취향을 가진 특수한 모든 분들을 위해 인사 드립니다.
-        </p>
-        <p>
-          게임은 또다른 세계를 만드는 즐거운 일입니다. 
-        </p>
-        <p>
-          그리고 그 즐거운 일을 아직도 하고 있다는 것에 언제나 감사하고 있습니다. 
-        </p>
-        <p>
-          부디 저희가 만든 세계 안에서 일반적인 행복과 특수한 행복 모두를 얻으시길 간절히 바랍니다. 
-          여러분! 젠틀매니악은 안전합니다!
-        </p>
-        <p>
-          p.s 일반인 여러분들도 너무 걱정하지 마십시오.
-        </p>
-
-
-        <br /><br /><br />
-        <h3>이태웅 대표</h3>
-        <br />
-        <p>게임업계에는 게임이 정말 좋아서 이 일을 시작한 사람이 많아요.</p>
-        <p>
-          그런데 일하다 보면 내가 좋아하고 만들고 싶은 게임을 만들 기회는 정말
-          적어요.
-        </p>
-        <p>
-          인생이란 게, 사회생활이란 게 내가 하고 싶은 것만 하고 살 수는
-          없더라고요. 하지만 인생의 긴 여정에서 우리 회사에서 일한 시간이 내가
-          하고 싶은 일을 했던 시간, 덕업일치를 이룬 시간, 애쓰지 않아도 일이
-          잘되는 그런 시간이 되었으면 좋겠어요.
-        </p>
-        <p>
-          자신의 인생에서 그런 시간을 가질 수 있다면 의미가 있다고 생각해요.
-          우리 구성원들이 그런 시간을 여기서 가질 수 있다면, 창업하길 잘했다고
-          큰 보람을 느낄 것 같아요. 저희는 그런 회사를 만들기 위해서 계속 노력할
-          거고요.
-        </p>
-
-        
+        <animated.h1 ref={titleRef} style={titleStyle}>
+          인사말
+        </animated.h1>
+        <div>
+          {greetingData.map((item, index) => (
+            <div>
+              <GreetingItem key={index} item={item} index={index} />
+              <br/>
+            </div>
+          ))}
+        </div>
       </Wrapper>
     </SectionLayout>
   );
 };
+
 
 const Wrapper = styled.div`
   height: 100%;
   justify-content: flex-start;
   align-items: flex-start;
   margin-top: 10%;
+`;
 
-  h1 {
+
+// MARK: Greeting Item
+const GreetingItem = ({ item, index }) => {
+  return (
+    <ItemWrapper>
+      <div className="icon">
+        <Image src={item.icon} alt={`${index}-icon`} />
+      </div>
+      
+      <div>
+        <h2>{item.title}</h2>
+        <br/>
+        <p>{item.content}</p>
+      </div>
+    </ItemWrapper>
+  );
+};
+
+const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  .icon {
     @media ${device.mobile} {
-      margin-bottom: 2.5rem;
-      padding: 0 0.5rem 0.5rem;
+      display: none;
     }
+    flex-shrink: 0;
+    width: 300px;
+    margin-right: 24px;
+  }
 
-    display: inline-block;
-    padding: 0 1rem 1rem;
-    margin-bottom: 4rem;
-    border-bottom: 2px solid;
+  p {
+    font-size: 14pt;
+    @media ${device.mobile} {
+      font-size: 12pt;
+    }
   }
 `;
+
+
+
+export const greetingData = [
+  {
+    icon: Img.대표님01,
+    title: "복규동 대표",
+    content: "일반 취향을 가진 일반인 분들과 특수 취향을 가진 특수한 모든 분들을 위해 인사 드립니다. 게임은 또다른 세계를 만드는 즐거운 일입니다. 그리고 그 즐거운 일을 아직도 하고 있다는 것에 언제나 감사하고 있습니다. 부디 저희가 만든 세계 안에서 일반적인 행복과 특수한 행복 모두를 얻으시길 간절히 바랍니다. 여러분! 젠틀매니악은 안전합니다! p.s 일반인 여러분들도 너무 걱정하지 마십시오."
+  },
+  {
+    icon: Img.대표님02,
+    title: "이태웅 대표",
+    content:
+      "게임업계에는 게임이 정말 좋아서 이 일을 시작한 사람이 많아요. 그런데 일하다 보면 내가 좋아하고 만들고 싶은 게임을 만들 기회는 정말 적어요. 인생이란 게, 사회생활이란 게 내가 하고 싶은 것만 하고 살 수는 없더라고요. 하지만 인생의 긴 여정에서 우리 회사에서 일한 시간이 내가 하고 싶은 일을 했던 시간, 덕업일치를 이룬 시간, 애쓰지 않아도 일이 잘되는 그런 시간이 되었으면 좋겠어요. 자신의 인생에서 그런 시간을 가질 수 있다면 의미가 있다고 생각해요. 우리 구성원들이 그런 시간을 여기서 가질 수 있다면, 창업하길 잘했다고 큰 보람을 느낄 것 같아요. 저희는 그런 회사를 만들기 위해서 계속 노력할거고요."
+  }
+];
